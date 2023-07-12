3f34275fe9ec073fbfead5d783b425cce6b303ad
rolins2 <127750238+rolins2@users.noreply.github.com>
2023-07-13 02:48:57 +0530

    Initial com

--- quizcheck.js
+++ quizcheck.js
@@ -2,7 +2,7 @@ const quizForm = document.querySelector(".quizform");
 
 const submitbtn = document.querySelector("#submitbtn");
 
-submitbtn.addEventListener("click",function clickHandler(){
+submitbtn.addEventListener("click", function clickHandler() {
     console.log("button clicked ")
 
     checkScore();
@@ -10,42 +10,39 @@ submitbtn.addEventListener("click",function clickHandler(){
 
 const output = document.querySelector("#output");
 
-const rightAnswers = ["90","Rightangled"];
+const rightAnswers = ["90", "Rightangled"];
 
-function checkScore(){
+function checkScore() {
     var score = 0;
     var i = 0;
-    
+
     const formResults = new FormData(quizForm);
 
-    
-    for(let val of formResults.values())
-    {
-        if(val === rightAnswers[i]){
-            score = score +1;
-       
+
+    for (let val of formResults.values()) {
+        if (val === rightAnswers[i]) {
+            score = score + 1;
+
 
         }
-      
-        i = i+1;
+
+        i = i + 1;
     }
 
     printScore(score);
-   
-    
+
+
 
 }
 
-function printScore(s)
-{
-    if (s===2){
-        output.innerText = "Your score is " +s + " you got everything right";
-    }
-    else if(s===1){
-        output.innerText = "Your score is " +s+ " you got one question right";
+function printScore(s) {
+    if (s === 2) {
+        output.innerText = "Your score is " + s + " you got everything right";
+    } else if (s === 1) {
+        output.innerText = "Your score is " + s + " you got one question right";
 
-    }else{
-        output.innerText = "Your score is " +s + " you got both the questions wrong";
+    } else {
+        output.innerText = "Your score is " + s + " you got both the questions wrong";
 
     }
 }
---
+++ tenuse.js
@@ -0,0 +1,11 @@
+const val1 = document.querySelector("#val1");
+
+const submitbtn = document.querySelector("#submitbtn");
+
+submitbtn.addEventListener("click",function clcikHandler(){
+    
+   // calculateHypotenuse();
+     console.log(val1);
+   console.log("Button clicked");
+    
+})
